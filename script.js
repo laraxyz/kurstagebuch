e0e1390d2b4526b50c556d64ff27cda83e994720
Sebastian Binder <sebastian.binder@nice-solutions.de>
2024-01-09 10:26:16 +0100

    3 änderungen

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Path: script.js Änderung 2
+
 document.getElementById("changeTextButton").addEventListener("click", function() {
     document.getElementById("header").textContent = "Neuer Text";
 });
+
+
